b45e073d44126b81adfb8a89a5eabdb95837e96c
Ardis Lu <ardisglu@gmail.com>
2022-06-23 21:15:31 -0700

    Update dependencies

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { marked } from 'https://cdn.jsdelivr.net/npm/marked@4.0.12/lib/marked.esm.min.js';
-import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/es/highlight.min.js';
-import powershell from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/es/languages/powershell.min.js';
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked@4.0.17/lib/marked.esm.min.js';
+import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/es/highlight.min.js';
+import powershell from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/es/languages/powershell.min.js';
 hljs.registerLanguage('powershell', powershell);
 
 navigator.serviceWorker.register('/sw.js');
