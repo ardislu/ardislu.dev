d8d1ced0cb47fbf192444b94ac564e6be8bdbe43
Ardis Lu <ardisglu@gmail.com>
2022-07-20 19:49:37 -0700

    Block API calls to non-Google domains

--- functions/api.js
+++ functions/api.js
@@ -2,10 +2,16 @@ import { getGoogleAuthToken } from './auth.js';
 
 export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
-  const queryString = decodeURIComponent(url.search.substring(1));
+  const queryUrl = new URL(decodeURIComponent(url.search.substring(1)));
+
+  // To prevent service account token from leaking to non-Google domains
+  if (!(queryUrl.hostname === 'sheets.googleapis.com' || queryUrl.hostname === 'docs.googleapis.com')) {
+    return new Response('Invalid query URL. Query URL hostname must be "sheets.googleapis.com" or "docs.googleapis.com".', { status: 400 });
+  }
+
   const token = await getGoogleAuthToken(env.EMAIL, env.PRIVATE_KEY, env.SCOPES);
 
-  const response = await fetch(queryString, {
+  const response = await fetch(queryUrl, {
     cf: {
       cacheEverything: true,
       cacheTtl: 86400
