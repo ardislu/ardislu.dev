5ead72ad822c321ed6f7bdb2a92734f17643ea94
Ardis Lu <ardisglu@gmail.com>
2022-07-20 19:40:01 -0700

    Simplify variables

--- functions/api.js
+++ functions/api.js
@@ -3,12 +3,7 @@ import { getGoogleAuthToken } from './auth.js';
 export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
   const queryString = decodeURIComponent(url.search.substring(1));
-
-  const email = env.EMAIL // E.g. "example@example.com"
-  const privateKey = env.PRIVATE_KEY // E.g. "-----BEGIN PRIVATE KEY-----\n...EXAMPLE...\n-----END PRIVATE KEY-----\n"
-  const scopes = env.SCOPES // E.g. "https://example.com/auth/scope1 https://example.com/auth/scope2"
-
-  const token = await getGoogleAuthToken(email, privateKey, scopes);
+  const token = await getGoogleAuthToken(env.EMAIL, env.PRIVATE_KEY, env.SCOPES);
 
   const response = await fetch(queryString, {
     cf: {
