431ce9a2ae9696ee00d01510093f0ecdb460afef
Ardis Lu <ardisglu@gmail.com>
2022-11-11 20:32:35 -0800

    Add Solidity syntax highlighting

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ import FlexDocument from 'https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.7
 import { marked } from 'https://cdn.jsdelivr.net/npm/marked@4.0.18/lib/marked.esm.min.js';
 import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/es/highlight.min.js';
 import powershell from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/es/languages/powershell.min.js';
+import solidity from '/highlightjs-solidity.min.js';
 hljs.registerLanguage('powershell', powershell);
+hljs.registerLanguage('solidity', solidity);
 
 /* Set up stale-while-revalidate cache strategy: if there's anything in the cache, return that immediately.
   Wait for the origin server's response in parallel and show the refresh toast if the cache changed. */
