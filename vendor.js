75e5fb79b3fc61c323606bfa17ac39fac162d02d
Ardis Lu <ardisglu@gmail.com>
2023-09-17 14:37:02 -0700

    Bundle JS dependencies instead of using CDN
    Introduces minimal Node.js/npm dependency but allows better tree-shaking and minification

---
+++ vendor.js
@@ -0,0 +1,7 @@
+import FlexDocument from 'flexsearch/dist/module/document';
+import { marked } from 'marked';
+import hljs from 'highlight.js';
+import powershell from 'highlight.js/lib/languages/powershell';
+import { solidity } from 'highlightjs-solidity';
+
+export { FlexDocument, marked, hljs, powershell, solidity };
